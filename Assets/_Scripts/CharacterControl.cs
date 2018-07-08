356eeb10e0823ed5cc252092d023d24e810f37a2
Intelix® <nikolat-repin201303@mail.ru>
2018-07-08 17:07:57 +0700

    Fixed camera, animations, added tilemap

--- Assets/_Scripts/CharacterControl.cs
+++ Assets/_Scripts/CharacterControl.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 
 public class CharacterControl : MonoBehaviour {
 
-    Vector2 move, mouse;
-
-    Vector3 centerPoint;
+    Vector3 move, mouse, centerPoint;
 
     [SerializeField]
     GameObject[] legs = new GameObject[2];
@@ -36,29 +34,26 @@ public class CharacterControl : MonoBehaviour {
         else
             transform.eulerAngles = new Vector3(0, 0, ToDeg(Mathf.Atan(mouse.y / mouse.x)) - 90);
 
-        cam.transform.position = transform.position + new Vector3(0, 0, -5);
+        cam.transform.position = transform.position + new Vector3(0, 0, -15) + mouse / 225;
 
-        legs[0].transform.localPosition = Vector3.Lerp(new Vector3(0.3f, 0, 0), new Vector3(0.3f, -0.5f, 0), legCycle);
-        legs[1].transform.localPosition = Vector3.Lerp(new Vector3(-0.25f, 0, 0), new Vector3(-0.25f, -0.5f, 0), 1 - legCycle);
+        legs[0].transform.localPosition = Vector3.Lerp(new Vector3(0.3f, 0, 1), new Vector3(0.3f, -0.5f, 1), legCycle);
+        legs[1].transform.localPosition = Vector3.Lerp(new Vector3(-0.25f, 0, 1), new Vector3(-0.25f, -0.5f, 1), 1 - legCycle);
     }
 
-    float ToDeg(float radians)
-    {
-        return radians * (180 / Mathf.PI);
-    }
+    float ToDeg(float radians) { return radians * (180 / Mathf.PI); }
 
     IEnumerator legCycleLerp()
     {
         while (true)
         {
-            for (float i = 0; i <= 1; i += 0.075f)
+            for (float i = 0; i <= 1; i += Mathf.Lerp(0, 0.075f, move.magnitude))
             {
                 legCycle = i;
 
                 yield return new WaitForSeconds(0.01f);
             }
 
-            for (float i = 1; i >= 0; i -= 0.075f)
+            for (float i = 1; i >= 0; i -= Mathf.Lerp(0, 0.075f, move.magnitude))
             {
                 legCycle = i;
 
